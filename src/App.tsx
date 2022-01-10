7265ae7c8934f95b8435e14b5be345b0d160b717
andrea-emmanuele <andrea@mono.studio>
2022-01-10 18:12:02 +0100

    feat: Textbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss'
+import Textbox from "./components/Textbox/Textbox";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.login}>
+      <h1 className={classes.login__headline}>Log in</h1>
+      <form className={classes.login__form}>
+        <Textbox label={'Login, email or phone number'} />
+        <input type="password" />
+      </form>
     </div>
   );
 }
