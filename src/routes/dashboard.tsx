809a96fa0f6093f80e347fc6d27df4dd4511cf4b
andrea-emmanuele <andrea@mono.studio>
2022-01-11 18:18:15 +0100

    feat: protected route dashboard

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {Navigate} from "react-router-dom";
+import {useTypedSelector} from "../hooks/storeHooks";
 
-const Dashboard = () => (
-    <main>Welcome!</main>
-);
+const Dashboard = () => {
+    const token: string = useTypedSelector(state => state.auth.userToken)
+    return token
+            ? <div> logged in </div>
+            : <Navigate to="/" replace />
+};
 
 export default Dashboard;
