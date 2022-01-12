5420bca876831e5b364591bb64d02bcc71cc35a6
andrea-emmanuele <andrea@mono.studio>
2022-01-12 14:08:45 +0100

    fix(AuthGuard): typed props

--- src/components/AuthGuard/AuthGuard.tsx
+++ src/components/AuthGuard/AuthGuard.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import {useTypedDispatch} from "../../hooks/storeHooks";
 import {checkExistingUserToken} from "../../store/auth";
 
-const AuthGuard: React.FC = ({children}) => {
+interface AuthGuardProps {
+    children: React.ReactNode;
+}
+
+const AuthGuard: React.FC<AuthGuardProps> = ({children}) => {
     const dispatch = useTypedDispatch()
     dispatch(checkExistingUserToken())
 
